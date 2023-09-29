639db4c68ed9500c4076c946bfb02879fe2193c0
Bhushan Kalvani <19436859+bhushankalvani@users.noreply.github.com>
2023-09-30 05:09:14 +0530

    feature/screencast turn server (#1)
    * peer connection set and stream received. Play pending, stream change error prevents playing.
    
    * remote and local stream implementation to video tags
    
    * explicit mediastream object conversion added for Remote stream.
    
    * Remote stream does not work due to ICE candidate gathering state resets to 'new' after 'complete'
    
    * removed case for remote offer and reverted ice candidate method.
    
    * metered.ca turn server config setup
    
    * connection close handled
    
    * TURN server implementation led to final working of screencast. Actual IPs resolved for peer connections and WebRTC implementation works.

--- ipcRenderer-1.js
+++ ipcRenderer-1.js
@@ -1,38 +1,33 @@
-const { ipcRenderer, screen } = require('electron')
+const { ipcRenderer, desktopCapturer } = require('electron')
 
 console.log('ipcRenderer loaded');
 
+/** 
+ * @note Use your own TURN server config.
+ * Link for the provider I used for creating a quick and utilising an existing TURN server capability. 
+ * @link https://www.metered.ca/
+ */
+const ICE_CONFIG = require('./ice-server-config.json');
+const RTCICEConfig = ICE_CONFIG;
 
-/** @debug effects on resources */
-// contextBridge.exposeInMainWorld("ipcRenderer",ipcRenderer)
-
-// WebRTC default TURN server from many examples.
-const RTCConfig = {
-	iceServers: [{
-		'urls': 'stun:stun.l.google.com:19302',
-		username: 'webrtc',
-		credential: 'turnserver'
-	}]
-};
-// const RTCConfig = {
-// 	iceServers: [{
-// 			urls: "http://localhost:3000",  // A TURN server using socket.io
-// 			// username: "webrtc",
-// 			// credential: "turnserver"
-// 		}
-// 	]
-// };
 let PeerConnection;
+let IceCandidateQueue = [];
 
-
-/** @note create request */
+/** 
+ * @note create request from one client for the other.
+ * This example creates a request from peer id '2' in main.js for peer id '1'
+ */
 ipcRenderer.on('REQUEST_SCREENCAST', RequestScreencast);
 
-async function RequestScreencast() {
+async function RequestScreencast(event, sourceId, ScreenSize) {
 	console.log('method::REQUEST_SCREENCAST, ipcRenderer');
-	createPeerConnection();
+	if (!PeerConnection) {
+		createPeerConnection();
+	}
+
 	const offer = await PeerConnection.createOffer();
 	if (PeerConnection.signalingState != "stable") {
+		CloseScreencast();
 		return;
 	}
 
@@ -41,132 +36,356 @@ async function RequestScreencast() {
 	ipcRenderer.send('NEW_SCREENCAST_REQ', { sdp: JSON.stringify(PeerConnection.localDescription) });
 }
 
-ipcRenderer.on('REQUEST_RECEIVED', AcceptRTCRequest);
 
-async function AcceptRTCRequest(event, message) {
-	console.log('method::REQUEST_RECEIVED, ipcRenderer');
-	createPeerConnection();
-	await PeerConnection.setRemoteDescription(new RTCSessionDescription(JSON.parse(message.sdp)));
-	const answer = await PeerConnection.createAnswer();
-	console.log('answer', answer);
-	await PeerConnection.setLocalDescription(answer);
+/** 
+ * @note Negotiation event between peers to decide the best protocol for the call.
+ * and sends all queued ICE Candidates. New connections can also be established
+ * in case of old ICE candidate failures.
+ */
+ipcRenderer.on('NEGOTIATION_REQUEST_RECEIVED', NegotiationEvent);
+async function NegotiationEvent() {
+	// console.log('method::NEGOTIATION_REQUEST_RECEIVED, ipcRenderer');
+	if (!PeerConnection) {
+		createPeerConnection();
+	}
+
+	if (PeerConnection.signalingState != "stable") {
+
+		/** 
+		 * @note Set the local and remove descriptions for rollback;
+		 * don't proceed until both return.
+		 */
+		await Promise.all([
+			PeerConnection.setLocalDescription({ type: "rollback" }),
+			PeerConnection.setRemoteDescription(new RTCSessionDescription(JSON.parse(message.sdp)))
+		]);
+		return;
+	} else {
+		await PeerConnection.setRemoteDescription(new RTCSessionDescription(JSON.parse(message.sdp)));
+	}
+
+	await PeerConnection.setLocalDescription(await PeerConnection.createAnswer());
+
 	/** @note send data using sockets */
-	ipcRenderer.send('ACCEPT_INVITE', { sdp: JSON.stringify(answer) });
+	ipcRenderer.send('ACCEPT_INVITE', { sdp: JSON.stringify(PeerConnection.localDescription) });
+	
+	/** @note After setting the local description, check if there are ICE candidates in the queue. */
+	sendQueuedICECandidates();
 }
 
+/** 
+ * @note Actual request received from peer using socket server.
+ * Accepting call here.
+ */
+ipcRenderer.on('REQUEST_RECEIVED', AcceptRTCRequest);
+async function AcceptRTCRequest(event, message, sourceId, ScreenSize) {
+	// console.log('method::REQUEST_RECEIVED, ipcRenderer');
+	if (!PeerConnection) {
+		createPeerConnection();
+	}
 
-ipcRenderer.on('RECORD_SCREEN', ShareScreen);
+	if (PeerConnection.signalingState != "stable") {
+		/** 
+		 * @note Set the local and remove descriptions for rollback; 
+		 * don't proceed until both return.
+		 */
+		await Promise.all([
+			PeerConnection.setLocalDescription({ type: "rollback" }),
+			PeerConnection.setRemoteDescription(new RTCSessionDescription(JSON.parse(message.sdp)))
+		]);
+		return;
+	} else {
+		await PeerConnection.setRemoteDescription(new RTCSessionDescription(JSON.parse(message.sdp)));
+	}
 
-async function ShareScreen(event, sourceId, ScreenSize) {
-	console.log('method::RECORD_SCREEN, ipcRenderer');
-    /** @fixme remove creating peer connection and call the global method createPeerConnection() instead. */
-	/** @fixme remove RTCSignaling and replace with socket call for receiving peer call offer. */
-    // RTCSignalingChannel.addEventListener('message', async message => {
-		// if (message.offer) {
-			// PeerConnection.setRemoteDescription(new RTCSessionDescription(message.offer));
-			// const answer = await PeerConnection.createAnswer();
-            // console.log('answer', answer);
-			// await PeerConnection.setLocalDescription(answer);
-			// /** @note send data using sockets */
-            // ipcRenderer.send('accept-invite', { answer });
-            try {
-				const stream = await navigator.mediaDevices.getUserMedia({
-				  audio: false,
-				  video: {
-					mandatory: {
-					  chromeMediaSource: 'desktop',
-					  /** @fixme sourceId required */
-					  chromeMediaSourceId: sourceId,
-					  minWidth: ScreenSize.width,
-					  maxWidth: ScreenSize.width,
-					  minHeight: ScreenSize.height,
-					  maxHeight: ScreenSize.height,
-					}
-				  }
-				});
-				stream.getTracks().forEach(async (track) => {
-					await PeerConnection.addTrack(track, stream);
-				});
-			  } catch (e) {
-				console.log('error in screencast', e);
-			  }
-		// }
-	// });
+	try {
+		const streams = await navigator.mediaDevices.getUserMedia({
+			audio: false,
+			video: {
+				mandatory: {
+					chromeMediaSource: 'desktop',
+					chromeMediaSourceId: sourceId,
+					minWidth: ScreenSize.width,
+					maxWidth: ScreenSize.width,
+					minHeight: ScreenSize.height,
+					maxHeight: ScreenSize.height,
+				}
+			}
+		});
+
+		
+		streams.getTracks().forEach(async (track) => {
+			await PeerConnection.addTrack(track, streams);
+		});
+
+		// console.log('handle local streams', streams);
+		handleLocalStream(streams);
+
+		await PeerConnection.setLocalDescription(await PeerConnection.createAnswer());
+
+		/** @note send data using sockets */
+		ipcRenderer.send('ACCEPT_INVITE', { sdp: JSON.stringify(PeerConnection.localDescription) });
+		
+		/** @note After setting the local description, check if there are ICE candidates in the queue. */
+		sendQueuedICECandidates();
+	} catch (error) {
+		console.log('stream error', error);
+	}
 }
 
 
 /**
- * @link https://github.com/mdn/samples-server/blob/master/s/webrtc-from-chat/chatclient.js#L348
- * @link https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection
+ * @note Method created to handle the successful peer connection answer.
+ * 'SHARE_SCREEN' since I use this example to share display source like screen sharing.
+ */
+ipcRenderer.on('SHARE_SCREEN', ShareScreen);
+async function ShareScreen(event, peerAnswer) {
+	// console.log('method::SHARE_SCREEN, ipcRenderer');
+	try {
+		if (PeerConnection.signalingState != 'stable') {
+			const answer = new RTCSessionDescription(JSON.parse(peerAnswer.sdp));
+			await PeerConnection.setRemoteDescription(answer);
+			// setPeerConnectionListeners();
+			sendQueuedICECandidates();
+		}
+	} catch (e) {
+		console.error('error in screencast', e);
+	}
+}
+
+/** 
+ * @note Creating a WebRTC peer connection and registering all required and
+ * some extra events for WebRTC peer monitoring.
  */
 function createPeerConnection() {
-	PeerConnection = new RTCPeerConnection(RTCConfig);
+	PeerConnection = new RTCPeerConnection(RTCICEConfig);
 
-    /** @fixme Pending event handlers */
-    PeerConnection.onicecandidate = handleICECandidateEvent;
-    PeerConnection.oniceconnectionstatechange = handleICEConnectionStateChangeEvent;
-    PeerConnection.onicegatheringstatechange = handleICEGatheringStateChangeEvent;
-    PeerConnection.onsignalingstatechange = handleSignalingStateChangeEvent;
-    PeerConnection.ontrack = handleTrackEvent;
-    PeerConnection.onnegotiationneeded = RequestScreencast;
+	PeerConnection.onicecandidate = onCreateNewICECandidateEvent;
+	PeerConnection.oniceconnectionstatechange = handleICEConnectionStateChangeEvent;
+	PeerConnection.onicegatheringstatechange = handleICEGatheringStateChangeEvent;
+	PeerConnection.onsignalingstatechange = handleSignalingStateChangeEvent;
+	PeerConnection.ontrack = handleTrackEvent;
+	PeerConnection.onnegotiationneeded = handleNegotiationNeededEvent;
+	PeerConnection.onconnectionstatechange = handleConnectionStateChangeEvent;
 }
 
-function handleTrackEvent(event) {
-	console.log("*** Track event");
-	// document.getElementById("received_video").srcObject = event.streams[0];
-	// document.getElementById("hangup-button").disabled = false;
-
-
+/**
+ * @note Can check current and different connection states
+ * for a peer connection (here PeerConnection).
+ */
+async function handleConnectionStateChangeEvent(event) {
+	if (PeerConnection.connectionState === 'connected') {
+		// Peers connected!
+		console.info('Peers connected');
+    }
 }
 
+/** 
+ * @note Handle different tracks added by remote peer.
+ * I use it to transfer remote stream to my video element in angular index.html
+ */
+async function handleTrackEvent(event) {
+	// console.log('remote streams', event.streams);
+	const remoteStream = event.streams[0];
+	handleRemoteStream(remoteStream);
+}
+
+/**
+ *  @note I used it to monitor and learn about different states during ICE candidacy exchange
+ *  and different states a connection goes through on both ends. 
+ */
 function handleICEGatheringStateChangeEvent(event) {
-	console.log("*** ICE gathering state changed to: " + PeerConnection.iceGatheringState);
+	console.log("*o* ICE gathering state changed to: " + PeerConnection.iceGatheringState);
 }
 
-// Handle |iceconnectionstatechange| events. This will detect
-// when the ICE connection is closed, failed, or disconnected.
-//
-// This is called when the state of the ICE agent changes.
+/** Handle |iceconnectionstatechange| events. This will detect
+ * when the ICE connection is closed, failed, or disconnected.
+ *
+ * This is called when the state of the ICE agent changes.
+ */
 function handleICEConnectionStateChangeEvent(event) {
-	console.log("*** ICE connection state changed to " + PeerConnection.iceConnectionState);
-  
-	switch(PeerConnection.iceConnectionState) {
-	  case "closed":
-	  case "failed":
-	  case "disconnected":
-		// closeVideoCall();
-		break;
-	}
-}
+	console.log("*o* ICE connection state changed to " + PeerConnection.iceConnectionState);
 
-// Set up a |signalingstatechange| event handler. This will detect when
-// the signaling connection is closed.
-//
-// NOTE: This will actually move to the new RTCPeerConnectionState enum
-// returned in the property RTCPeerConnection.connectionState when
-// browsers catch up with the latest version of the specification!
-function handleSignalingStateChangeEvent(event) {
-	console.log("*** WebRTC signaling state changed to: " + PeerConnection.signalingState);
-	switch(PeerConnection.signalingState) {
+	switch (PeerConnection.iceConnectionState) {
 		case "closed":
-			/** @fixme Add ipcEvent to tell socket to end connections and clean up. */
-			// closeVideoCall();
-		break;
+		case "failed":
+		case "disconnected":
+			console.log('*o* ICE candidate call failed');
+			CloseScreencast();
+			break;
 	}
 }
 
-// Handles |icecandidate| events by forwarding the specified
-// ICE candidate (created by our local ICE agent) to the other
-// peer through the signaling server.
-
-function handleICECandidateEvent(event) {
-	if (event.candidate) {
-	  console.log("*** Outgoing ICE candidate: " + event.candidate.candidate);
-  
-	//   sendToServer({
-	// 	type: "new-ice-candidate",
-	// 	target: targetUsername,
-	// 	candidate: event.candidate
-	//   });
+/**
+ * @note Set up a |signalingstatechange| event handler. This will detect when
+ * the signaling connection is closed.
+ * 
+ * This will actually move to the new RTCPeerConnectionState enum
+ * returned in the property RTCPeerConnection.connectionState when
+ * browsers catch up with the latest version of the specification!
+ */
+function handleSignalingStateChangeEvent(event) {
+	console.log("*o* WebRTC signaling state changed to: " + PeerConnection.signalingState);
+	switch (PeerConnection.signalingState) {
+		case "closed":
+			CloseScreencast();
+			console.log('*o* ICE candidate call closed');
+			break;
 	}
 }
+
+/**
+ * @note Handles |icecandidate| events by forwarding the specified
+ * ICE Candidate (created by our local ICE agent) to the other
+ * peer through the signaling server.
+ */ 
+function onCreateNewICECandidateEvent(event) {
+	/** 
+	 * @note managing a queue until remote description is set
+	 * to prevent ICE Candidates from failure.
+	 */
+	if(event.candidate) {
+		if (!PeerConnection.remoteDescription) {
+			IceCandidateQueue.push(event.candidate);
+		} else {
+			sendIceCandidateToPeer(event.candidate);
+		}
+	}
+}
+
+/**
+ * @note Sends all the ICE Candidates from the queue
+ * when remote description is set. 
+ */
+function sendQueuedICECandidates() {
+	if (PeerConnection.remoteDescription) {
+		while (IceCandidateQueue.length > 0) {
+		  const candidate = IceCandidateQueue.shift();
+		  sendIceCandidateToPeer(candidate);
+		}
+	  }
+}
+
+/**
+ * @note Sends individual candidates over socket to the connected Peer.
+*/
+function sendIceCandidateToPeer(candidate) {
+	ipcRenderer.send('ICE_CANDIDATE', { candidate: JSON.stringify(candidate) });
+}
+
+/**
+ * @note Handle received ICE Candidate from Peer over socket.
+ */
+ipcRenderer.on('NEW_ICE_CANDIDATE', handleReceivedICECandidateMsg);
+async function handleReceivedICECandidateMsg(event, request) {
+	try {
+		if (!PeerConnection.remoteDescription || !PeerConnection.localDescription) return;
+		const candidate = new RTCIceCandidate(JSON.parse(request.candidate));
+
+		// console.log("*o* Adding received ICE candidate: ", candidate);
+		await PeerConnection.addIceCandidate(candidate);
+	} catch (err) {
+		console.error('error adding ice candidate', err);
+	}
+}
+
+/** @note handle video streams */
+/** @note handle local streams */
+function handleLocalStream(stream) {
+	const localVideo = document.getElementById('localVideo');
+	localVideo.autoplay = true;
+	localVideo.srcObject = stream;
+	localVideo.onloadedmetadata = (e) => localVideo.play();
+}
+
+/** @note handle remote streams */
+function handleRemoteStream(stream) {
+	const remoteVideo = document.getElementById('remoteVideo');
+	localVideo.autoplay = true;
+	remoteVideo.srcObject = stream;
+	remoteVideo.onloadedmetadata = (e) => remoteVideo.play();
+}
+
+/**
+ * @note Handle PeerConnection negotiation event.
+ * Used to finalise protocols for peer connections for sending data.
+ */
+async function handleNegotiationNeededEvent() {
+	// console.log("Negotiation needed");
+
+	try {
+		const offer = await PeerConnection.createOffer();
+
+		/** 
+		 * @note If the connection hasn't yet achieved the "stable" state,
+		 * return to the caller. Another negotiationneeded event
+		 * will be fired when the state stabilizes.
+		 */
+
+		if (PeerConnection.signalingState != "stable") {
+			console.log("The connection isn't stable yet; postponing...")
+			return;
+		}
+
+		/** @note Establish the offer as the local peer's current description. */
+		// console.log("Setting local description to the offer");
+		await PeerConnection.setLocalDescription(offer);
+
+		/** @note Send the offer to the remote peer. */
+		// console.log("Sending the offer to the remote peer");
+		ipcRenderer.send('NEGOTIATION', { sdp: JSON.stringify(PeerConnection.localDescription) });
+
+	} catch (err) {
+		console.error("The following error occurred while handling the negotiationneeded event:", err);
+	};
+}
+
+/**
+ * @note Safely unload peer connection and stop it's streams.
+ * Use it to hangup calls normally or in errored states.
+ */
+async function CloseScreencast() {
+	if (PeerConnection) {
+		// console.log("Closing connection and call");
+	
+		/** 
+		 * @note Disconnect all our event listeners; we don't want stray events
+		 * to interfere with the hangup while it's ongoing.
+		 */
+		PeerConnection.ontrack = null;
+		PeerConnection.onnicecandidate = null;
+		PeerConnection.oniceconnectionstatechange = null;
+		PeerConnection.onsignalingstatechange = null;
+		PeerConnection.onicegatheringstatechange = null;
+		PeerConnection.onnotificationneeded = null;
+	
+		/** @note Stop all transceivers on the connection */
+		PeerConnection.getTransceivers().forEach(transceiver => {
+		  transceiver.stop();
+		});
+	
+		/**
+		 * @note Stop the webcam preview as well by pausing the <video>
+		 * element, then stopping each of the getUserMedia() tracks
+		 * on it.
+	 	 */
+		if (localVideo.srcObject) {
+		  localVideo.pause();
+		  localVideo.srcObject.getTracks().forEach(track => {
+			track.stop();
+		  });
+		}
+	
+		/** @note Close the peer connection */
+	
+		PeerConnection.close();
+		PeerConnection = null;
+	}
+}
+
+/**
+ * @references WebRTC examples.
+ * @link https://webrtc.org/getting-started/remote-streams
+ * @link https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection
+ * @link https://github.com/mdn/samples-server/blob/master/s/webrtc-from-chat/chatclient.js
+ */
---
+++ main.js
@@ -0,0 +1,176 @@
+const { app, BrowserWindow, screen, desktopCapturer, ipcMain } = require('electron');
+const url = require('url');
+const path = require('path');
+
+let AppWindow;
+let ScreenSize;
+
+// Socket.io
+const { io } = require("socket.io-client");
+const socket = io(
+	'http://localhost:3000',
+	{
+		transports: ['websocket', 'polling'],
+	}
+);
+
+/** @fixme Enable UUIDv4 implementation. */
+// Get the UUID v4 for each connection.
+// function uuidv4() {
+//     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+//     .replace(/[xy]/g, function (c) {
+//         const r = Math.random() * 16 | 0, 
+//             v = c == 'x' ? r : (r & 0x3 | 0x8);
+//         return v.toString(16);
+//     });
+// }
+
+/** @fixme UUID v4 implementation pending for conditionals below */
+// const id = uuidv4();
+const id = 2; /** @note Static user id to request and respond to screencast requests */
+
+function onReady() {
+	console.log('device UUID v4', id);
+	const primaryDisplay = screen.getPrimaryDisplay();
+	ScreenSize = primaryDisplay.workAreaSize;
+
+	AppWindow = new BrowserWindow({
+		// titleBarStyle: id === 1 ? 'hidden' : 'default',
+		width: ScreenSize.width,
+		height: ScreenSize.height,
+		devTools: true,
+		nodeIntegration: true,
+		webPreferences: {
+			preload: path.join(app.getAppPath(), 'ipcRenderer-1.js'),
+			nodeIntegration: true,
+			devTools: true,
+		}
+	});
+
+	AppWindow.loadURL(
+		url.format({
+			pathname: path.join(
+				__dirname,
+				'dist/index.html'),
+			protocol: 'file:',
+			slashes: true
+		})
+	);
+
+	AppWindow.setTitle(`Candidate #${id}`)
+
+	setupSocketConnections();
+}
+
+function setupSocketConnections() {
+	socket.on('connect', SocketConnect);
+	socket.on('request-screencast', ScreencastRequest);
+	socket.on('disconnect', SocketDisconnect);
+	socket.on('screencast-accepted', ScreencastReqAccepted);
+	socket.on('ice-candidate-received', HandleIceCandidateReceiveEvent);
+	socket.on('negotiation-request', NegotiationRequestReceived);
+
+	socket.connect();
+}
+
+async function NegotiationRequestReceived(peerReq) {
+	AppWindow.webContents.send('NEGOTIATION_REQUEST_RECEIVED', peerReq);	
+}
+
+function SocketConnect() {
+	// console.log('socket connected');
+	socket.emit('register-user', { id }, async (res) => {
+		/** @note requesting a screencast for default user with userId 1. */
+		/** @fixme Use UUIDv4 implementation and make it a dynamic implementation. */
+		if (id === 2) {
+			// console.log(`user id ${id} requesting screencast for 1`);
+			const sources = await desktopCapturer.getSources({ types: ['window', 'screen'] });
+			for (const source of sources) {
+				if (source.name === 'Entire screen') {
+					AppWindow.webContents.send('REQUEST_SCREENCAST', source.id, ScreenSize);
+					return;
+				}
+			}
+		}
+	});
+
+}
+
+/**
+ * @note Request for screencast for this user from a peer.
+ * @param {peerId} - Receives the peer Id requesting a call.
+ */
+async function ScreencastRequest(peerReq) {
+	// console.log('screencast request received', peerReq, id);
+	const sources = await desktopCapturer.getSources({ types: ['window', 'screen'] });
+	for (const source of sources) {
+		if (source.name === 'Entire screen') {
+			AppWindow.webContents.send('REQUEST_RECEIVED', peerReq, source.id, ScreenSize);
+			return;
+		}
+	}
+}
+
+async function ScreencastReqAccepted(peerAnswer) {
+	// console.log('screencast request accepted');
+	const sources = await desktopCapturer.getSources({ types: ['screen'] });
+	AppWindow.webContents.send('SHARE_SCREEN', peerAnswer);
+}
+
+function HandleIceCandidateReceiveEvent(request) {
+	AppWindow.webContents.send('NEW_ICE_CANDIDATE', request);
+}
+
+function SocketDisconnect() {
+	// console.log('disconnect fired');
+	socket.emit('disconnect-call', 1);
+	socket.disconnect();
+}
+
+/** @note Event handlers for requests from ipcRenderer to main thread. */
+/** @fixme Use UUIDv4 implementation and make it a dynamic implementation. */
+ipcMain.on('NEW_SCREENCAST_REQ', (event, request) => {
+	if (id === 2) {
+		request['by'] = 2;
+		request['for'] = 1;
+		socket.emit('request-screencast-cl', request);
+	}
+});
+
+/** @fixme Use UUIDv4 implementation and make it a dynamic implementation. */
+ipcMain.on('NEGOTIATION', (event, request) => {
+	if (id === 2) {
+		request['by'] = 2;
+		request['for'] = 1;
+	} if(id === 1) {
+		request['by'] = 1;
+		request['for'] = 2;
+	}
+	socket.emit('negotiation', request);
+});
+
+/** @fixme Use UUIDv4 implementation and make it a dynamic implementation. */
+ipcMain.on('ACCEPT_INVITE', (event, acceptedInvite) => {
+	if(id === 1) {
+		acceptedInvite['by'] = 1;
+		acceptedInvite['for'] = 2;
+	} else {
+		acceptedInvite['by'] = 2;
+		acceptedInvite['for'] = 1;
+	}
+	socket.emit('accepted-invite', acceptedInvite);
+});
+
+/** @fixme Use UUIDv4 implementation and make it a dynamic implementation. */
+ipcMain.on('ICE_CANDIDATE', (event, request) => {
+	if (id === 1) { /** @note @fixme Just for test. Use real user ids for 'by' and 'for' fields when implementing. */
+		request['by'] = 1;
+		request['for'] = 2;
+	} else {
+		request['by'] = 2;
+		request['for'] = 1;
+	}
+	socket.emit('new-ice-candidate', request);
+});
+
+app.on('ready', onReady);
